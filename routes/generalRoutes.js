6a6691eac4c3997fda86631acb7c8aa6b3e1b35e
vindahs <vincentonoja@ymail.com>
2022-04-05 08:06:32 -0700

    first commit

--- routes/generalRoutes.js
+++ routes/generalRoutes.js
@@ -2,80 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('99786', function(req, res, next) {
+    res.render('99786', { title: 'Express' });
 });
 
-router.get('/about', function(req, res, next) {
-    res.render('about', { title: 'Express' });
+router.get('/98687', function(req, res, next) {
+    res.render('98687', { title: 'Express' });
 });
 
-router.get('/contact', function(req, res, next) {
-    res.render('contact', { title: 'Express' });
+router.get('/99878', function(req, res, next) {
+    res.render('99878', { title: 'Express' });
 });
 
-router.get('/news', function(req, res, next) {
-    res.render('news', { title: 'Express' });
-});
-
-router.get('/news1', function(req, res, next) {
-    res.render('news1', { title: 'Express' });
-});
-
-router.get('/news2', function(req, res, next) {
-    res.render('news2', { title: 'Express' });
-});
-
-router.get('/news3', function(req, res, next) {
-    res.render('news3', { title: 'Express' });
-});
-
-router.get('/news4', function(req, res, next) {
-    res.render('news4', { title: 'Express' });
-});
-
-router.get('/news5', function(req, res, next) {
-    res.render('news5', { title: 'Express' });
-});
-
-router.get('/1', function(req, res, next) {
-    res.render('1', { title: 'Express' });
-});
-
-router.get('/2', function(req, res, next) {
-    res.render('2', { title: 'Express' });
-});
-
-router.get('/3', function(req, res, next) {
-    res.render('3', { title: 'Express' });
-});
-
-router.get('/4', function(req, res, next) {
-    res.render('4', { title: 'Express' });
-});
-
-router.get('/5', function(req, res, next) {
-    res.render('5', { title: 'Express' });
-});
-
-router.get('/6', function(req, res, next) {
-    res.render('6', { title: 'Express' });
-});
-
-router.get('/7', function(req, res, next) {
-    res.render('7', { title: 'Express' });
-});
-
-router.get('/8', function(req, res, next) {
-    res.render('8', { title: 'Express' });
-});
-
-router.get('/9', function(req, res, next) {
-    res.render('9', { title: 'Express' });
-});
-
-router.get('/10', function(req, res, next) {
-    res.render('10', { title: 'Express' });
+router.get('/99556', function(req, res, next) {
+    res.render('99556', { title: 'Express' });
 });
 
 module.exports = router;
